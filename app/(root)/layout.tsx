883962b88b83d4a340b265473e868e91f6cac329
hariyebk <harunbekri6@gmail.com>
2023-11-26 17:04:19 +0300

    UI layouts

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -2,6 +2,7 @@ import { ClerkProvider } from '@clerk/nextjs'
 import '../globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { TopBar, BottomBar, RightSideBar, LeftSideBar } from '@/components/shared'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,7 +20,17 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={inter.className}>
-          {children}
+          <TopBar />
+            <main className='flex flex-row'>
+              <LeftSideBar />
+                  <section className='main-container'>
+                    <div className='w-full max-w-4xl'>
+                        {children}
+                    </div>
+                  </section>
+              <RightSideBar />
+            </main>
+          <BottomBar />
         </body>
       </html>
     </ClerkProvider>
